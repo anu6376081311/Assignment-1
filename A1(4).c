06f89ef30c84135d4fee12dd207a2967490d8291
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-13 01:08:30 +0530

    Add files via upload

---
+++ A1(4).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int R;
+    float A;
+    printf("Enter the radias of the circle :");
+    scanf("%d",&R);
+    A=3.14*R*R;
+    printf("Area of circle is A having radius R is %f",A);
+    return 0;
+}
